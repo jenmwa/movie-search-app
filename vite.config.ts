b3e576225fa966256fc1f68e82de539432af627f
jenmwa <jenmwa@gmail.com>
2023-05-22 15:51:40 +0200

    vite.config.ts update

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '', /*github.com/jenmwa/ VÅR BASE*/
+  base: '/movie-search-app/',
   define: {
     'process.env': {},
   },
